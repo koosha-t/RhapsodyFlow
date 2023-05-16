81687e32e85294b197b26903a4882028066cd074
Koosha Tahmasebipour <kooshi.ml@gmail.com>
2023-05-15 21:48:38 -0400

    adding a simple test

--- src/rhapsody_agent.py
+++ src/rhapsody_agent.py
@@ -1,4 +1,5 @@
 import re
+import pdb
 
 def fraction_to_float(fraction):
     numerator, denominator = fraction.split('/')
@@ -19,9 +20,9 @@ def find_violating_measures(time_signature, melocode):
                 beat_sum += fraction_to_float(duration)
             
             if beat_sum != beat_sum_target:
-                violating_measures[index] = measure
+                violating_measures[index-1] = measure
                 break
-                
+    
     return violating_measures
 
 
@@ -30,3 +31,22 @@ def count_measures(melocode):
     measures = re.findall(measure_pattern, melocode)
     return len(measures)
 
+
+
+if __name__ == "__main__":
+    score ="""
+(4/4) | <p> 1/4C4 1/4E4 1/4G4 1/4C5 && 1/4C2 1/4G2 1/4C3 1/4G3 |
+1/4[E2,G2,B2,E3] R3/4 && 1/4[E3,G3,B3,E4] R3/4 |
+1/8E4 1/16G4 1/16B4 1/8E5 1/16G5 1/16B5 R1/2 && 1/4[E2,G2,B2,E3] R1/2 |
+1/8G4 1/16B4 1/16D5 1/8G5 1/16B5 1/16D6 R1/2 && 1/4[G2,B2,D3,G3] R1/2 |
+1/8E4 1/16G4 1/16B4 1/8E5 1/16G5 1/16B5 R1/2 && 1/4[E2,G2,B2,E3] R1/2 |
+R1/2 1/4E4 1/4G4 1/4B4 && 1/4E2 1/4G2 1/4B2 R1/4 |
+1/4G4 1/4B4 1/4D5 && 1/4G2 1/4B2 1/4D3 R1/4 |
+1/4E4 1/4G4 1/4B4 && 1/4E2 1/4G2 1/4B2 R1/4 |
+1/8E4 1/16G4 1/16B4 1/8E5 1/16G5 1/16B5 R1/2 && 1/4[E2,G2,B2,E3] R1/2 |
+<f> 1/4E4 1/4G4 1/4B4 && 1/4E2 1/4G2 1/4B2 R1/4 |
+1/4G4 1/4B4 1/4D5 && 1/4G2 1/4B2 1/4D3 R1/4 |
+1/4E4 1/4G4 1/4B4 && 1/4E2 1/4G2 1/4B2 R1/4 |
+1/4[E2,G2,B2,E3] R3/4 && 1/4[E3,G3,B3,E4] R3/4 | """
+    
+    print(find_violating_measures('4/4',score))
